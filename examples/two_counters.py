d3146b947842aa9f76ab3ec5d81e38a000b0c877
Bruce Eckel <mindviewinc@gmail.com>
2026-05-09 14:46:43 -0600

    Update two_counters.py

--- examples/two_counters.py
+++ examples/two_counters.py
@@ -10,7 +10,7 @@ a: int = 0
 b: int = 0
 
 
-def go(iterations: int) -> None:
+def two_counters(iterations: int) -> None:
     global a, b
     for _ in range(iterations):
         a += 1
@@ -20,7 +20,7 @@ def go(iterations: int) -> None:
 if __name__ == "__main__":
     print(gil_info())
 
-    run_threads(go, (c.ITERATIONS,))
+    run_threads(two_counters, (c.ITERATIONS,))
 
     ok = a == b
     show_status("a == b", f"a={a:,}  b={b:,}", ok)
